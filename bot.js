506912b1f764422e39f88430c097ca7853be705e
Nicholas Ryan <nnryanp@gmail.com>
2019-03-24 05:06:41 -0700

    Cleaned up code, moved token to json file.

--- bot.js
+++ bot.js
@@ -1,11 +1,12 @@
+const token = require("./token.json");
 const Discord = require('discord.js');
 const {Storage} = require('@google-cloud/storage');
 var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+
 const storage = new Storage();
 const client = new Discord.Client();
-const bucket = storage.bucket('damocles_tatoebot');
 
-function UrlExists(url){
+function doesUrlExists(url){
   var http = new XMLHttpRequest();
   http.open('HEAD', url, false);
   http.send();
@@ -19,10 +20,123 @@ async function getNhentaiNumbersAsync(channel) {
       cursedNumber = cursedNumber + "" + Math.floor(Math.random()*9);
     }
     var url = "https://nhentai.net/g/"+cursedNumber+"/";
-    if(UrlExists(url)){
+    if(doesUrlExists(url)){
       channel.send(url);
       break;
     }
+  }
+  return;
+}
+
+function spank(msg, args){
+  if(!args.length){
+    storage.bucket('damocles_tatoebot').getFiles(function(err, files) {
+      if (!err) {
+        files[Math.floor(Math.random()*files.length)].getSignedUrl({
+          action: 'read',
+          expires: '03-09-2491'
+        }).then(signedUrls => {
+          const url = signedUrls[0];
+          var built = url.split('?');
+          msg.channel.send(built[0]);
+        });
+      }
+    });
+  }
+  return;
+}
+
+function roll(msg, args){
+  if (args.length == 1){
+    if(parseInt(args[0]) > 0){
+      msg.reply(Math.floor((Math.random() * parseInt(args[0])) + 1));
+      return;
+    }
+  }else if(args.length == 2){
+    if((parseInt(args[0]) > 0) && (parseInt(args[0]) < 13)){
+      if(parseInt(args[1]) > 0){
+        var message = "\n";
+        for (var i = 0; i < parseInt(args[0]); i++){
+          message = message + Math.floor((Math.random() * parseInt(args[1])) + 1)+"\n";
+        }
+        if(message != ""){
+          msg.reply(message);
+          return;
+        }
+      }
+    }
+  }
+  return;
+}
+
+function clear(msg, args){
+  if (args.length == 1){
+    if((parseInt(args[0]) > 0) && (parseInt(args[0]) < 40)){
+      msg.channel.bulkDelete(parseInt(args[0])+1);
+    }
+  }
+  return;
+}
+
+function shitpost(msg, args){
+  if(!args.length){
+    storage.bucket('tatoebot_shitpost').getFiles(function(err, files) {
+      if (!err) {
+        files[Math.floor(Math.random()*files.length)].getSignedUrl({
+          action: 'read',
+          expires: '03-09-2491'
+        }).then(signedUrls => {
+          const url = signedUrls[0];
+          var built = url.split('?');
+          msg.channel.send(built[0]);
+          return;
+        });
+      }
+    });
+  }
+  return;
+}
+
+function deletethis(msg, args){
+  if(args.length == 1){
+    if(args[0] === 'this'){
+      msg.channel.send('https://storage.googleapis.com/tatoebot_resources/deleteit.jpg');
+    }
+  }
+  return;
+}
+
+function help(msg, args){
+  if(!args.length){
+    msg.channel.send(" - Roll\n ( Roll [number of sides] ) or ( Roll [number of dice] [number of sides] ) \n \n - Spank \n Generates a random entry from the spankbank. \n \n - bammer \n Showcases the ban hammer. \n \n - kys \n Do it, it's painless. \n \n - nun \n Say the line bart. \n \n - dab \n hit the hard dab. \n \n - clear \n ( clear [amount of lines] : does not count the clear message line.)");
+  }
+  return;
+}
+
+function bammer(msg, args){
+  if(!args.length){
+    msg.channel.send('https://storage.googleapis.com/tatoebot_resources/HomerHammer.gif');
+  }
+  return;
+}
+
+function kys(msg, args){
+  if(!args.length){
+    msg.channel.send('https://www.youtube.com/watch?v=4gO7uemm6Yo');
+  }
+  return;
+}
+
+function nun(msg, args){
+  if(!args.length){
+    msg.channel.send('https://storage.googleapis.com/tatoebot_resources/Nun.png');
+  }
+  return;
+}
+
+function dab(msg, args){
+  if(!args.length){
+    msg.channel.send('https://storage.googleapis.com/tatoebot_resources/ClearcutDisastrousFieldmouse.webm');
   }
   return;
 }
@@ -38,104 +152,8 @@ client.on('message', msg => {
   const args = msg.content.split(' ');
   const command = args.shift().toLowerCase();
 
-  if (command === 'roll') {
-    if (!args.length) {
-      return;
-    }
-    if (args.length == 1){
-      var number = parseInt(args[0]);
-      msg.reply(Math.floor((Math.random() * number) + 1));
-      return;
-    }else if(args.length == 2){
-
-      var message = "\n";
-
-      if((parseInt(args[0]) == -1) || (parseInt(args[1]) == -1) || (parseInt(args[0]) > 13) || (parseInt(args[1]) == 0)){
-        return;
-      }
-
-      for (var i = 0; i < parseInt(args[0]); i++){
-        message = message + Math.floor((Math.random() * parseInt(args[1])) + 1)+"\n";
-      }
-
-      if(message != ""){
-        msg.reply(message);
-      }
-
-    }else{
-      return;
-    }
-  }
-
-  if (command === 'spank'){
-    if(!args.length){
-      bucket.getFiles(function(err, files) {
-        if (!err) {
-          files[Math.floor(Math.random()*files.length)].getSignedUrl({
-            action: 'read',
-            expires: '03-09-2491'
-          }).then(signedUrls => {
-            const url = signedUrls[0];
-            var built = url.split('?');
-            msg.channel.send(built[0]);
-          });
-        }
-      });
-    }
-    return;
-  }
-
-  if(command === 'bammer'){
-    if(!args.length){
-      msg.channel.send('https://storage.googleapis.com/tatoebot_resources/HomerHammer.gif');
-    }
-    return;
-  }
-
-  if(command === "kys"){
-    if(!args.length){
-      msg.channel.send('https://www.youtube.com/watch?v=4gO7uemm6Yo');
-    }
-    return;
-  }
-
-  if(command === 'nun'){
-    if(!args.length){
-      msg.channel.send('https://storage.googleapis.com/tatoebot_resources/Nun.png');
-    }
-    return;
-  }
-
-  if(command === 'dab'){
-    if(!args.length){
-      msg.channel.send('https://storage.googleapis.com/tatoebot_resources/ClearcutDisastrousFieldmouse.webm');
-    }
-  }
-
-  if(command === 'clear'){
-    if(!args.length){
-      return;
-    }else if (args.length == 1){
-      if((parseInt(args[0]) == -1) || (parseInt(args[1]) == -1) || (parseInt(args[0]) > 40) || (parseInt(args[1]) == 0)){
-        return;
-      }
-      msg.channel.bulkDelete(parseInt(args[0])+1);
-
-    }else{
-      return;
-    }
-  }
-
-  if(command === 'help'){
-    if(!args.length){
-      var message = " - Roll\n ( Roll [number of sides] ) or ( Roll [number of dice] [number of sides] ) \n \n - Spank \n Generates a random entry from the spankbank. \n \n - bammer \n Showcases the ban hammer. \n \n - kys \n Do it, it's painless. \n \n - nun \n Say the line bart. \n \n - dab \n hit the hard dab. \n \n - clear \n ( clear [amount of lines] : does not count the clear message line.)";
-      msg.channel.send(message);
-    }
-  }
-
   if(command.startsWith("[") && command.endsWith("]")){
-    var numbers = command.replace("[", "");
-    numbers = numbers.replace("]", "");
+    var numbers = command.replace("[", "").replace("]", "");
     if(numbers === "random"){
       getNhentaiNumbersAsync(msg.channel);
       return;
@@ -146,8 +164,42 @@ client.on('message', msg => {
         return;
       }
     }
+    return;
   }
 
+  switch(command){
+    case 'roll':
+      roll(msg, args);
+      break;
+    case 'spank':
+      spank(msg, args);
+      break;
+    case 'clear':
+      clear(msg, args);
+      break;
+    case 'shitpost':
+      shitpost(msg, args);
+      break;
+    case 'delete':
+      deletethis(msg, args);
+      break;
+    case 'help':
+      help(msg, args);
+      break;
+    case 'bammer':
+      bammer(msg, args);
+      break;
+    case 'kys':
+      kys(msg, args);
+      break;
+    case 'nun':
+      nun(msg, args);
+      break;
+    case 'dab':
+      dab(msg, args);
+      break;
+  }
+  return;
 });
 
-client.login('token');
+client.login(token.token);
