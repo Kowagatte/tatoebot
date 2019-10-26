5ae17109320ea40e83171814b88012ca933f4f8d
Nick Ryan <nnryanp@gmail.com>
2019-10-26 12:56:03 -0600

    fixed inspire

--- commands/inspire.js
+++ commands/inspire.js
@@ -7,7 +7,7 @@ module.exports = {
         const options = new URL('https://inspirobot.me/api?generate=true');
         let request = https.get((options), (res) =>{
             res.on('data', (d) => {
-                message.channel.send(d);
+                message.channel.send(d.toString());
             });
         });
     },
