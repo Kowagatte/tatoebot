af3ccdd1a6164b78ce4b4a3b3a9bc6fe16321d4e
Nick Ryan <nnryanp@gmail.com>
2021-09-05 23:09:41 -0600

    Fixed the args setup for the nhentai command

--- commands/nhentai.js
+++ commands/nhentai.js
@@ -5,20 +5,24 @@ module.exports = {
     customTrigger: {flag: true, prefix: '[', suffix: ']'},
     description: 'Returns an nhentai link',
     execute(message, args) {
-        if(args === "random"){
-            const options = new URL("https://nhentai.net/random/");
-            let request = https.get((options), (res) => {
-                message.channel.send(`https://nhentai.net${res.headers.location}`);
-            });
+        if(args.length === 1){
+            if(args[0] === "random"){
+                const options = new URL("https://nhentai.net/random/");
+                let request = https.get((options), (res) => {
+                    message.channel.send(`https://nhentai.net${res.headers.location}`);
+                });
+            }else{
+                const options = new URL(`https://nhentai.net/g/${args}/`);
+                let request = https.get((options), (res) =>{
+                    if(res.statusCode !== 404){
+                        message.channel.send(`https://nhentai.net/g/${args}/`);
+                    }else{
+                        message.channel.send("That Hentai doesn't exist.")
+                    }
+                });
+            }
         }else{
-            const options = new URL(`https://nhentai.net/g/${args}/`);
-            let request = https.get((options), (res) =>{
-               if(res.statusCode !== 404){
-                   message.channel.send(`https://nhentai.net/g/${args}/`);
-               }else{
-                   message.channel.send("That Hentai doesn't exist.")
-               }
-            });
+            message.channel.send("Improper use of the nhentai command.")
         }
     },
 };
